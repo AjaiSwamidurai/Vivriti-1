f7cfa76334d2017c69e326528519ba6390829125
Ajay283 <84388501+Ajay283@users.noreply.github.com>
2022-11-09 22:38:14 +0530

    Added

---
+++ src/main/java/com/example/employeemnagerapp/EmployeemnagerappApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeemnagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemnagerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemnagerappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employeemnagerapp/EmployeemnagerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeemnagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemnagerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
